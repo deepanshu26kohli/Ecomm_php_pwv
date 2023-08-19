26bcd60f3d98aa32d9dd69d9860ba33921e19699
Deepanshu Kohli <deepanshukohli63@gmail.com>
2023-08-19 09:40:49 +0530

    admin panel setup + login logout done

---
+++ inc.files/functions.inc.php
@@ -0,0 +1,15 @@
+<?php
+function pr($arr){
+    echo '<pre>';
+    print_r($arr);
+}
+function prx($arr){
+    echo '<pre>';
+    print_r($arr);
+    die();
+}
+function get_safe_val($con,$val){
+ if($val != ""){
+    return mysqli_real_escape_string($con,$val);
+ }}
+?>
